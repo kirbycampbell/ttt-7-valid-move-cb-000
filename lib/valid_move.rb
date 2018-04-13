2accbff4a304c3f8fba8d958c844db4fe11263a8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-13 14:51:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if position_taken? == true
      false
-  if board[index] => 0 && board[index] =< 9
+  if board[index] >= 0 && board[index] <= 9
      false
   else
      true
